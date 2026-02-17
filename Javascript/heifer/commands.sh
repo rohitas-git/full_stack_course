2f275befcbc678bc74aabed64882a61e0d9b037d
Rohitas Bansal <bansalrohitas44@gmail.com>
2026-02-17 11:11:02 +0530

    Learned about Strict Mode

--- Javascript/heifer/commands.sh
+++ Javascript/heifer/commands.sh
@@ -8,3 +8,19 @@ npm i -D @babel/core @babel/cli @babel/preset-env
 
 # Use Babel to transpile your main.js file to an older version of JavaScript:
 npx babel main.js --out-file main.compiled.js
+
+# To enable strict mode, you just need to "use strict" at the top of your file:
+# "use strict";
+# --- your file ---
+
+# You can also enable strict mode for a single function:
+# function strictFunction() {
+#   "use strict";
+# }
+
+# Here's the best part: you only need "use strict"; 
+# at the top of non-es6 modules. 
+# ES6 modules are always in strict mode by default. 
+# That goes for the browser and Node.js.
+
+# ES6 modules use the import and export keywords instead of require and module.exports.
---
+++ Javascript/heifer/strict.js
@@ -0,0 +1,5 @@
+"use strict";
+message = "moo!";
+console.log(message);
+
+// This will throw an error because we are using strict mode.
